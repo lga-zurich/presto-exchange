26b932884a8b491dc27d100835cb7c8f02420709
Daniel Bauer <dnb@zurich.ibm.com>
2025-10-10 09:19:33 +0200

    Fix shutdown of velox UCX communicator thread in PrestoServer.cpp
    (cherry picked from commit 6e5a4ec854d82b17af35928567c7029aa30516e3)

--- presto-native-execution/presto_cpp/main/PrestoServer.cpp
+++ presto-native-execution/presto_cpp/main/PrestoServer.cpp
@@ -193,14 +193,12 @@ void unregisterVeloxCudf(std::shared_ptr<std::thread> serverThread) {
   if (systemConfig->values().contains(
           facebook::velox::cudf_velox::CudfConfig::kCudfEnabled) &&
       facebook::velox::cudf_velox::CudfConfig::getInstance().enabled) {
-    auto server = facebook::velox::cudf_exchange::Communicator::initAndGet(SystemConfig::instance()->cudfServerPort(),nullptr);
-    if (server) {
-      server->stop();
-      server.reset();
-    }
     facebook::velox::cudf_velox::unregisterCudf();
     PRESTO_SHUTDOWN_LOG(INFO) << "cuDF is unregistered.";
     if (serverThread) {
+      auto server = facebook::velox::cudf_exchange::Communicator::getInstance();
+      server->stop();
+      server.reset();      
       PRESTO_SHUTDOWN_LOG(INFO) << "Joining UCX Communicator thread for shutdown.";
       serverThread->join();
     }
