aee4f3dd629f28a2ef958149baf55415a92fcaaf
Zoltan Arnold Nagy <nag@zurich.ibm.com>
2025-11-05 00:01:40 +0100

    translate the OutputNode to the rootFragment

--- presto-native-execution/presto_cpp/main/types/PrestoToVeloxQueryPlan.cpp
+++ presto-native-execution/presto_cpp/main/types/PrestoToVeloxQueryPlan.cpp
@@ -2209,7 +2209,8 @@ core::PlanNodePtr VeloxQueryPlanConverterBase::toVeloxQueryPlan(
       node->id,
       toRowType(node->outputVariables, typeParser_),
       VectorSerde::Kind::kPresto,
-      toVeloxQueryPlan(node->source, tableWriteInfo, taskId));
+      toVeloxQueryPlan(node->source, tableWriteInfo, taskId),
+      true);
 }
 
 core::PlanNodePtr VeloxInteractiveQueryPlanConverter::toVeloxQueryPlan(
